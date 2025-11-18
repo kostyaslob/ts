c3dc6225acb16f95aec0beb43fc556767a5d66e8
kostyaslob <kostyaslob@gmail.com>
2025-11-18 16:45:17 +0200

    done task-8

--- src/task-8.ts
+++ src/task-8.ts
@@ -1,10 +1,16 @@
 import axios from "axios";
 
-async function fetchPosts() {
-  const response = await axios.get(
+interface Post {
+    id: number;
+    title: string;
+    body: string;
+}
+
+async function fetchPosts(): Promise<Post[]> {
+  const response = await axios.get<Post[]>(
     '<https://jsonplaceholder.typicode.com/posts>'
   );
-  return response.data;
+    return response.data;
 }
 
 fetchPosts().then((posts) => {
