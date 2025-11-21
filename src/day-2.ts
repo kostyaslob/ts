3f6688df38445ccb248251b38b859ae738f65418
kostyaslob <kostyaslob@gmail.com>
2025-11-21 17:50:53 +0200

    done day-2, block-2

--- src/day-2.ts
+++ src/day-2.ts
@@ -101,30 +101,59 @@
     
 // }
 
-function safeLength(value: unknown): number {
-    if (typeof value === "string") {
-        return value.length
-    }
-    if (Array.isArray(value)) {
-        return value.length
-    }
-    return 0;
+// function safeLength(value: unknown): number {
+//     if (typeof value === "string") {
+//         return value.length
+//     }
+//     if (Array.isArray(value)) {
+//         return value.length
+//     }
+//     return 0;
+// }
+
+// type Input = number | string | boolean | null | undefined;
+// function describeValue(value: Input): string {
+//     if (typeof value === "number") {
+//         return `This is a number: ${value}`
+//     }
+//     if (typeof value === "string") {
+//         return `This is a string: ${value}`
+//     }
+//     if (typeof value === "boolean") {
+//         return `This is ${value}`
+//     }
+//     return value === null
+//         ? "This is null"
+//         : value === undefined
+//             ? "This is undefined"
+//             : "Unknown"
+// }
+
+function concatStrings(a: string, b?: string): string {
+    return b ? a + b : a;
 }
 
-type Input = number | string | boolean | null | undefined;
-function describeValue(value: Input): string {
-    if (typeof value === "number") {
-        return `This is a number: ${value}`
-    }
-    if (typeof value === "string") {
-        return `This is a string: ${value}`
-    }
-    if (typeof value === "boolean") {
-        return `This is ${value}`
-    }
-    return value === null
-        ? "This is null"
-        : value === undefined
-            ? "This is undefined" 
-            : "Unknown"            
+function power(base: number, exponent: number = 2): number {
+    return base ** exponent;
 }
+
+type StringOperator = (a: string, b: string) => string;
+const concat: StringOperator = (a, b) => a + b;
+const interpolate: StringOperator = (a, b) => `${a}, ${b}!`
+
+
+function filterNumbers(arr: number[], predicate: (n: number) => boolean): number[]  {
+    return arr.filter(predicate);
+}
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const evenNumbers = filterNumbers(numbers, n => n % 2 === 0)
+console.log(evenNumbers);
+const oddNumbers = filterNumbers(numbers, n => n % 2 !== 0);
+console.log(oddNumbers);
+const greaterThanThree = filterNumbers(numbers, n => n > 3)
+console.log(greaterThanThree);
+
+const double = (n: number): number => n * 2
+
+
