9ff9489647f0fab506ece7131b5d90f98f940cd1
kostyaslob <kostyaslob@gmail.com>
2025-11-27 14:49:45 +0200

    done task DeepReadonly

--- src/day-6.ts
+++ src/day-6.ts
@@ -25,66 +25,93 @@
 // type OmitUser = Omit<User, "password">
 
 
-type Car = {
-  brand: string;
-  model: string;
-  mileage: number;
-};
+// type Car = {
+//   brand: string;
+//   model: string;
+//   mileage: number;
+// };
 
-const update: Partial<Car> = {mileage: 40}
-function applyUpdate(car: Car, update: Partial<Car>): Car {
-    return {...car, ...update}
-}
-
-
-type User = {
-  id: number;
-  name?: string;
-  email?: string;
-};
-
-type FullUser = Required<User>
-const newUser: FullUser = {
-    id: 40,
-    name: "Kostya",
-    email: "kkk@k.ua"
-}
-
-type Point = {
-  x: number;
-  y: number;
-};
-type ReadonlyPoint = Readonly<Point>
-function freezePoint(p: Point): Readonly<Point> {
-    return {...p}
-}
+// const update: Partial<Car> = {mileage: 40}
+// function applyUpdate(car: Car, update: Partial<Car>): Car {
+//     return {...car, ...update}
+// }
 
 
-type Product = {
-  id: number;
-  title: string;
-  price: number;
-  description: string;
-};
-type ProductPreview = Pick<Product, "id" | "title">
+// type User = {
+//   id: number;
+//   name?: string;
+//   email?: string;
+// };
 
-type ProductList = Pick<Product, "id" | "price">[]
-const productList: ProductList = [
-    { id: 40, price: 40 },
-    { id: 21, price: 50 },
-    { id: 1, price: 100 },
-]
+// type FullUser = Required<User>
+// const newUser: FullUser = {
+//     id: 40,
+//     name: "Kostya",
+//     email: "kkk@k.ua"
+// }
+
+// type Point = {
+//   x: number;
+//   y: number;
+// };
+// type ReadonlyPoint = Readonly<Point>
+// function freezePoint(p: Point): Readonly<Point> {
+//     return {...p}
+// }
 
 
-type Post = {
-  id: number;
-  title: string;
-  body: string;
-  author: string;
-};
-type WithoutBody = Omit<Post, "body">
+// type Product = {
+//   id: number;
+//   title: string;
+//   price: number;
+//   description: string;
+// };
+// type ProductPreview = Pick<Product, "id" | "title">
 
-function getPostPreview(post: Post): WithoutBody {
-    const { body, ...rest } = post;
-    return rest;
-}
+// type ProductList = Pick<Product, "id" | "price">[]
+// const productList: ProductList = [
+//     { id: 40, price: 40 },
+//     { id: 21, price: 50 },
+//     { id: 1, price: 100 },
+// ]
+
+
+// type Post = {
+//   id: number;
+//   title: string;
+//   body: string;
+//   author: string;
+// };
+// type WithoutBody = Omit<Post, "body">
+
+// function getPostPreview(post: Post): WithoutBody {
+//     const { body, ...rest } = post;
+//     return rest;
+// }
+
+// type Settings = {
+//   theme: string;
+//   layout: {
+//     header: boolean;
+//     footer: boolean;
+//   };
+// };
+
+// type DeepReadonly<T> = {
+//     readonly [K in keyof T]:
+//     T[K] extends object
+//     ? DeepReadonly<T[K]>
+//     : T[K]
+// };
+
+// const settings: DeepReadonly<Settings> = {
+//     theme: "dark",
+//     layout: {
+//         header: true,
+//         footer: false,
+//     }
+// }
+
+// settings.theme = "light";
+// settings.layout.footer = false;
+
